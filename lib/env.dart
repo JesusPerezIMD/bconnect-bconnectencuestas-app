5de5ac87176dc0288b12ca9ac861998d21cb53d5
Jesus Alejandro Perez Ramirez <Jperez@imasd.com.mx>
2024-02-28 12:32:00 -0600

    CAMBIOS

--- lib/env.dart
+++ lib/env.dart
@@ -5,14 +5,14 @@ class Environment {
   final GOOGLE_MAP_API_KEY = "AIzaSyDedLDHWHo6oEILIO5ycJYsv6dATa-Aaf8";
 
   //Local
-  final BCONNECT_API = "http://localhost:17330";
+  // final BCONNECT_API = "http://localhost:17330";
   // final ALIADOS_API = 'https://localhost:44376/api';
   // final UrlApiImages = 'https://localhost:44376/';
   // final TWILIO_API = 'http://localhost:7071/api';
   // final WHATSAPP_NUMBER = "15165185350";
 
   //Test
-  //final BCONNECT_API = "https://bconnectapitest.azurewebsites.net";
+  final BCONNECT_API = "https://bconnectapitest.azurewebsites.net";
   final ALIADOS_API = 'https://bconnect-aliados-api.azurewebsites.net/api';
   final UrlApiImages = 'https://bconnect-aliados-api.azurewebsites.net/';
   final TWILIO_API = 'https://twiliofunctionbconnect.azurewebsites.net/api';
@@ -28,8 +28,7 @@ class Environment {
   // final WHATSAPP_NUMBER = "5219993991528";
   
   final DEFAULT_LOCATION = const gm.LatLng(20.968147, -89.629872);
-  final SERVICE_ID = "34542C82-6659-4DA3-804B-1B62FB44EDSA"; //capacitacion
-  //final SERVICE_ID = "98E12E59-9555-4A1A-A20E-0324A06043D3";
+  final SERVICE_ID = "z593c9c6-b79f-4bf0-8ba2-fdb0a5345643";
   final SERVICE_NAME="BConnect Encuestas";
 
   static final Environment _environment = Environment._internal();
